f237e54cc03fc7afcb58e8c7e703deae0a9bbde8
Joshua Pirie <joshuapirie@gmail.com>
2015-12-14 17:48:51 +1030

    Day 14 solutions.

---
+++ 14-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _14_1 {
+    class Program {
+        static void Main(string[] args) {
+            int time = 2503;
+            List<int> reindeerDists = new List<int>();
+
+            string line;
+            StreamReader file = new StreamReader("input.txt");
+            while((line = file.ReadLine()) != null) {
+                GroupCollection matches = Regex.Match(line, @"^\w+ can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.").Groups;
+
+                int speed = int.Parse(matches[1].Value);
+                int duration = int.Parse(matches[2].Value);
+                int resting = int.Parse(matches[3].Value);
+                int leftover = time % (duration + resting);
+                int distance = time / (duration + resting) * duration * speed + ((leftover >= duration) ? duration * speed : leftover * speed);
+
+                reindeerDists.Add(distance);
+            }
+            file.Close();
+
+            Console.WriteLine(reindeerDists.Max());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 14-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _14_2 {
+    class Program {
+        static void Main(string[] args) {
+            int maxPoints = 0;
+            int time = 2503;
+            Dictionary<string, int[]> reindeerStats = new Dictionary<string, int[]>();
+
+            string line;
+            StreamReader file = new StreamReader("input.txt");
+            while((line = file.ReadLine()) != null) {
+                GroupCollection matches = Regex.Match(line, @"^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.").Groups;
+                reindeerStats[matches[1].Value] = new int[5] { 0, 0, int.Parse(matches[2].Value), int.Parse(matches[3].Value), int.Parse(matches[4].Value) };
+            }
+            file.Close();
+
+            for(int i = 0; i < time; i++) {
+                int maxDist = 0;
+                foreach(int[] stats in reindeerStats.Values) {
+                    if(i % (stats[3] + stats[4]) < stats[3])
+                        stats[1] += stats[2];
+                    if(stats[1] > maxDist)
+                        maxDist = stats[1];
+                }
+                foreach(int[] stats in reindeerStats.Values)
+                    if(stats[1] == maxDist)
+                        stats[0]++;
+            }
+
+            foreach(int[] stats in reindeerStats.Values)
+                if(stats[0] > maxPoints)
+                    maxPoints = stats[0];
+
+            Console.WriteLine(maxPoints);
+            Console.ReadLine();
+        }
+    }
+}
