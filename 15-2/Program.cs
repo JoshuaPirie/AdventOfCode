ae0ef49d664cd719a077d4f91b256b509f5fd111
Joshua Pirie <joshuapirie@gmail.com>
2015-12-16 04:22:44 +1030

    Day 15 optimized solutions.

--- 15-2/Program.cs
+++ 15-2/Program.cs
@@ -1,49 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace _15_2 {
     class Program {
         static void Main(string[] args) {
-            int bestScore = 0;
-            List<int[]> ingredientStats = new List<int[]>();
+            int maxScore = 0;
+            Dictionary<string, List<KeyValuePair<string, int>>> ingredients = new Dictionary<string, List<KeyValuePair<string, int>>>();
 
             string line;
             StreamReader file = new StreamReader("input.txt");
             while((line = file.ReadLine()) != null) {
-                GroupCollection matches = Regex.Match(line, @"^(\w+): \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+), \w+ (-?\d+)").Groups;
-                ingredientStats.Add(new int[5] {
-                    int.Parse(matches[2].Value),
-                    int.Parse(matches[3].Value),
-                    int.Parse(matches[4].Value),
-                    int.Parse(matches[5].Value),
-                    int.Parse(matches[6].Value)
-                });
+                GroupCollection matches = Regex.Match(line, @"^(\w+):(?: (\w+) (-?\d+),?)+").Groups;
+                ingredients[matches[1].Value] = new List<KeyValuePair<string, int>>();
+                for(int i = 0; i < matches[2].Captures.Count; i++)
+                    ingredients[matches[1].Value].Add(new KeyValuePair<string, int>(matches[2].Captures[i].Value, int.Parse(matches[3].Captures[i].Value)));
             }
             file.Close();
 
-            for(int i = 0; i <= 100; i++) {
-                for(int j = 0; j <= 100 - i; j++) {
-                    for(int k = 0; k <= 100 - i - j; k++) {
-                        int l = 100 - i - j - k;
-                        int capacity = i * ingredientStats[0][0] + j * ingredientStats[1][0] + k * ingredientStats[2][0] + l * ingredientStats[3][0];
-                        int durability = i * ingredientStats[0][1] + j * ingredientStats[1][1] + k * ingredientStats[2][1] + l * ingredientStats[3][1];
-                        int flavor = i * ingredientStats[0][2] + j * ingredientStats[1][2] + k * ingredientStats[2][2] + l * ingredientStats[3][2];
-                        int texture = i * ingredientStats[0][3] + j * ingredientStats[1][3] + k * ingredientStats[2][3] + l * ingredientStats[3][3];
-                        int calories = i * ingredientStats[0][4] + j * ingredientStats[1][4] + k * ingredientStats[2][4] + l * ingredientStats[3][4];
+            ExploreRecipies(ingredients, ingredients.First().Value.Count, new int[0], 100, ref maxScore);
 
-                        if(calories == 500 && capacity > 0 && durability > 0 && flavor > 0 && texture > 0) {
-                            int score = capacity * durability * flavor * texture;
-                            if(score > bestScore)
-                                bestScore = score;
-                        }
+            Console.WriteLine(maxScore);
+            Console.ReadLine();
+        }
+
+        static void ExploreRecipies(Dictionary<string, List<KeyValuePair<string, int>>> ingredients, int numStats, int[] quantities, int maxQuantity, ref int maxScore) {
+            if(quantities.Length == ingredients.Count - 1) {
+                int score = 1;
+                int[] subScores = new int[numStats];
+
+                for(int i = 0; i < subScores.Length; i++) {
+                    int counter = 0;
+                    foreach(List<KeyValuePair<string, int>> ingredient in ingredients.Values) {
+                        subScores[i] += ((counter == ingredients.Count - 1) ? maxQuantity - quantities.Sum() : quantities[counter]) * ingredient[i].Value;
+                        counter++;
                     }
                 }
-            }
 
-            Console.WriteLine(bestScore);
-            Console.ReadLine();
+                if(subScores[4] == 500) {
+                    for(int i = 0; i < subScores.Length - 1; i++) {
+                        if(subScores[i] < 0)
+                            subScores[i] = 0;
+                        score *= subScores[i];
+                    }
+
+                    if(score > maxScore)
+                        maxScore = score;
+                }
+            }
+            else {
+                int quantityLeft = maxQuantity - quantities.Sum();
+                for(int i = 0; i <= quantityLeft; i++) {
+                    int[] newQuantities = new int[quantities.Length + 1];
+                    quantities.CopyTo(newQuantities, 0);
+                    newQuantities[quantities.Length] = i;
+                    ExploreRecipies(ingredients, numStats, newQuantities, maxQuantity, ref maxScore);
+                }
+            }
         }
     }
 }
