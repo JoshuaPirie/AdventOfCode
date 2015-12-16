d74a139f459b0c120bd9d4ba3d0f7498effedabd
Joshua Pirie <joshuapirie@gmail.com>
2015-12-16 16:30:37 +1030

    Day 16 solutions.

---
+++ 16-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _16_1 {
+    class Program {
+        static void Main(string[] args) {
+            List<KeyValuePair<string, int>> results = new List<KeyValuePair<string, int>>();
+            int validAuntNum = 0;
+
+            string line;
+            StreamReader file = new StreamReader("input1.txt");
+            while((line = file.ReadLine()) != null) {
+                GroupCollection matches = Regex.Match(line, @"^(\w+): (\d+)").Groups;
+                results.Add(new KeyValuePair<string, int>(matches[1].Value, int.Parse(matches[2].Value)));
+            }
+            file.Close();
+
+            file = new StreamReader("input2.txt");
+            while((line = file.ReadLine()) != null) {
+                GroupCollection matches = Regex.Match(line, @"^Sue (\d+):(?: (\w+): (\d+),?)+").Groups;
+                bool containsCompound, validAunt = true;
+                for(int i = 0; i < matches[2].Captures.Count; i++) {
+                    containsCompound = false;
+                    foreach(KeyValuePair<string, int> result in results)
+                        if(result.Key == matches[2].Captures[i].Value && result.Value == int.Parse(matches[3].Captures[i].Value))
+                            containsCompound = true;
+                    if(!containsCompound)
+                        validAunt = false;
+                }
+                if(validAunt)
+                    validAuntNum = int.Parse(matches[1].Value);
+            }
+            file.Close();
+
+            Console.WriteLine(validAuntNum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 16-2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _16_1 {
+    class Program {
+        static void Main(string[] args) {
+            List<KeyValuePair<string, int>> results = new List<KeyValuePair<string, int>>();
+            int validAuntNum = 0;
+
+            string line;
+            StreamReader file = new StreamReader("input1.txt");
+            while((line = file.ReadLine()) != null) {
+                GroupCollection matches = Regex.Match(line, @"^(\w+): (\d+)").Groups;
+                results.Add(new KeyValuePair<string, int>(matches[1].Value, int.Parse(matches[2].Value)));
+            }
+            file.Close();
+
+            file = new StreamReader("input2.txt");
+            while((line = file.ReadLine()) != null) {
+                GroupCollection matches = Regex.Match(line, @"^Sue (\d+):(?: (\w+): (\d+),?)+").Groups;
+                bool containsCompound, validAunt = true;
+                for(int i = 0; i < matches[2].Captures.Count; i++) {
+                    containsCompound = false;
+                    foreach(KeyValuePair<string, int> result in results) {
+                        if(result.Key == matches[2].Captures[i].Value) {
+                            if(matches[2].Captures[i].Value == "cats" || matches[2].Captures[i].Value == "trees") {
+                                if(int.Parse(matches[3].Captures[i].Value) > result.Value)
+                                    containsCompound = true;
+                            }
+                            else if(matches[2].Captures[i].Value == "pomeranians" || matches[2].Captures[i].Value == "goldfish") {
+                                if(int.Parse(matches[3].Captures[i].Value) < result.Value)
+                                    containsCompound = true;
+                            }
+                            else if(int.Parse(matches[3].Captures[i].Value) == result.Value)
+                                containsCompound = true;
+                        }
+                    }
+                    if(!containsCompound)
+                        validAunt = false;
+                }
+                if(validAunt)
+                    validAuntNum = int.Parse(matches[1].Value);
+            }
+            file.Close();
+
+            Console.WriteLine(validAuntNum);
+            Console.ReadLine();
+        }
+    }
+}
