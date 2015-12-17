b7df6760456a8070667ceca80ab2423257e4304f
Joshua Pirie <joshuapirie@gmail.com>
2015-12-18 00:10:13 +1030

    Day 17 solutions.

---
+++ 17-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _17_1 {
+    class Program {
+        static void Main(string[] args) {
+            int counter = 0;
+            int quantity = 150;
+            List<int> containers = new List<int>();
+
+            string line;
+            StreamReader file = new StreamReader("input.txt");
+            while((line = file.ReadLine()) != null) {
+                containers.Add(int.Parse(line));
+            }
+            file.Close();
+
+            for(int i = 0; i < Math.Pow(2, containers.Count); i++) {
+                List<int> set = new List<int>();
+                for(int bits = i, j = 0; bits != 0; bits >>= 1, j++)
+                    if((bits & 1) != 0)
+                        set.Add(containers[j]);
+                if(set.Sum() == quantity)
+                    counter++;
+            }
+
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 17-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _17_2 {
+    class Program {
+        static void Main(string[] args) {
+            int counter = 0;
+            int minContainers = int.MaxValue;
+            int quantity = 150;
+            List<int> containers = new List<int>();
+
+            string line;
+            StreamReader file = new StreamReader("input.txt");
+            while((line = file.ReadLine()) != null) {
+                containers.Add(int.Parse(line));
+            }
+            file.Close();
+
+            for(int i = 0; i < Math.Pow(2, containers.Count); i++) {
+                List<int> set = new List<int>();
+                for(int bits = i, j = 0; bits != 0; bits >>= 1, j++)
+                    if((bits & 1) != 0)
+                        set.Add(containers[j]);
+                if(set.Sum() == quantity && set.Count < minContainers)
+                    minContainers = set.Count;
+            }
+
+            for(int i = 0; i < Math.Pow(2, containers.Count); i++) {
+                List<int> set = new List<int>();
+                for(int bits = i, j = 0; bits != 0; bits >>= 1, j++)
+                    if((bits & 1) != 0)
+                        set.Add(containers[j]);
+                if(set.Sum() == quantity && set.Count == minContainers)
+                    counter++;
+            }
+
+            Console.WriteLine(counter);
+            Console.ReadLine();
+        }
+    }
+}
