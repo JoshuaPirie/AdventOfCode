3f5d598dcbb31ae7783e78f53c5588732539d834
Joshua Pirie <joshuapirie@gmail.com>
2015-12-20 23:57:55 +1030

    Day 19 solutions.

---
+++ 19-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Text;
+
+namespace _19_1 {
+    class Program {
+        static void Main(string[] args) {
+            string input = "";
+            List<Tuple<string, string>> equations = new List<Tuple<string, string>>();
+            HashSet<string> results = new HashSet<string>();
+
+            bool lineBreak = false;
+            string line;
+            StreamReader file = new StreamReader("input.txt");
+            while((line = file.ReadLine()) != null) {
+                if(lineBreak)
+                    input = line;
+                else if(line == "")
+                    lineBreak = true;
+                else {
+                    GroupCollection matches = Regex.Match(line, @"(\w+) => (\w+)").Groups;
+                    equations.Add(Tuple.Create(matches[1].Value, matches[2].Value));
+                }
+            }
+            file.Close();
+
+            for(int i = 0; i < input.Length; i++) {
+                foreach(Tuple<string, string> equation in equations) {
+                    if(i + equation.Item1.Length <= input.Length && input.Substring(i, equation.Item1.Length) == equation.Item1) {
+                        StringBuilder builder = new StringBuilder(input.Substring(0, i));
+                        builder.Append(equation.Item2);
+                        builder.Append(input, i + equation.Item1.Length, input.Length - i - equation.Item1.Length);
+                        results.Add(builder.ToString());
+                    }
+                }
+            }
+
+            Console.WriteLine(results.Count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 19-2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Text;
+
+namespace _19_2 {
+    class Program {
+        static void Main(string[] args) {
+            string input = "";
+            string result = "e";
+            int steps = 0;
+            List<Tuple<string, string>> equations = new List<Tuple<string, string>>();
+
+            bool lineBreak = false;
+            string line;
+            StreamReader file = new StreamReader("input.txt");
+            while((line = file.ReadLine()) != null) {
+                if(lineBreak)
+                    input = line;
+                else if(line == "")
+                    lineBreak = true;
+                else {
+                    GroupCollection matches = Regex.Match(line, @"(\w+) => (\w+)").Groups;
+                    equations.Add(Tuple.Create(matches[1].Value, matches[2].Value));
+                }
+            }
+            file.Close();
+
+            TryFindCompound(equations, input, result, ref steps);
+
+            Console.WriteLine(steps);
+            Console.ReadLine();
+        }
+
+        static bool TryFindCompound(List<Tuple<string, string>> equations, string input, string result, ref int steps, int level = 0) {
+            if(input == result) {
+                steps = level;
+                return true;
+            }
+            else {
+                HashSet<string> replacements = new HashSet<string>();
+
+                for(int i = input.Length - 1; i >= 0; i--) {
+                    foreach(Tuple<string, string> equation in equations) {
+                        if(i + equation.Item2.Length <= input.Length && input.Substring(i, equation.Item2.Length) == equation.Item2) {
+                            StringBuilder builder = new StringBuilder(input.Substring(0, i));
+                            builder.Append(equation.Item1);
+                            builder.Append(input, i + equation.Item2.Length, input.Length - i - equation.Item2.Length);
+                            replacements.Add(builder.ToString());
+                        }
+                    }
+                }
+
+                foreach(string str in replacements)
+                    if(TryFindCompound(equations, str, result, ref steps, level + 1))
+                        return true;
+            }
+            return false;
+        }
+    }
+}
